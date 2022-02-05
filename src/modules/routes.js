4a1c66b749bda2435121ec8f69f38feef1d72044
Mominjon <78495004+Mominjon@users.noreply.github.com>
2022-02-05 10:20:34 +0500

    edit orta

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -8,75 +8,75 @@ const upload = require("./upload_files/upload_files")
 const router = express.Router()
 
 router
-    .post("/new_user", function (req, res, next) {
+    .post("/new_user",async function  (req, res, next) {
         const {token} = req.body
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
-    } ,  users.New_User)
-    .get("/users", function (req, res, next) {
-        const {token} = req.query
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
+    } , users.New_User)
+    .get("/users", async function  (req, res, next) {
+        const {token} = req.body
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
     } ,  users.Users)
-    .post("/delete_user", function (req, res, next) {
+    .post("/delete_user", async function  (req, res, next) {
         const {token} = req.body
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
     } ,  users.Delete_users)
     .post("/login_user", users.Login_user)
 
     // archives
 
-    .post("/new_archive", function (req, res, next) {
+    .post("/new_archive",async function  (req, res, next) {
         const {token} = req.body
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
-    } ,  archives.New_Archive)
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
+    } , archives.New_Archive)
     .get("/archives", archives.Archives)
     .get("/one_archive", archives.one_Archive)
-    .post("/update_archive", function (req, res, next) {
+    .post("/update_archive",async function  (req, res, next) {
         const {token} = req.body
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
     } ,  archives.Update_Archive)
 
     // articles
-    .post("/new_article", function (req, res, next) {
+    .post("/new_article", async function  (req, res, next) {
         const {token} = req.body
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
     } ,  articles.New_Article)
     .get("/articles", articles.Articles)
     .get("/one_articles", articles.One_Article)
-    .post("/delete_article", function (req, res, next) {
+    .post("/delete_article",async function  (req, res, next) {
         const {token} = req.body
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
-    } ,  articles.Delete_article)
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
+    } , articles.Delete_article)
 
     // paper
-    .post("/new_paper", function (req, res, next) {
+    .post("/new_paper",async function  (req, res, next) {
         const {token} = req.body
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
-    } ,  papers.New_Paper)
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
+    } , papers.New_Paper)
     .get("/papers", papers.Papers)
-    .post("/delete_paper", function (req, res, next) {
+    .post("/delete_paper",async function  (req, res, next) {
         const {token} = req.body
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
-    } ,  papers.Delete_paper)
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
+    } , papers.Delete_paper)
 
     // upload files
 
-    .post("/upload_img", function (req, res, next) {
-        const {token} = req.query
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
-    } ,  upload.Upload_img)
-    .post("/upload_file", function (req, res, next) {
+    .post("/upload_img", async function  (req, res, next) {
         const {token} = req.body
-        check.check_admin(token)
-        check == true ? next() : res.send("not admin")
-    } ,  upload.Upload_File)
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
+    } , upload.Upload_img)
+    .post("/upload_file", async function  (req, res, next) {
+        const {token} = req.body
+        let chesks = await check.check_admin(token)
+        chesks == true ? next() : res.send("not admin")
+    } , upload.Upload_File)
 module.exports = router
